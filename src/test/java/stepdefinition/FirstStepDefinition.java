b50e22b0b2c70c69bc1c9cc87b86356603edf9e0
umitkalemkus <umitkalemkus@yahoo.com>
2023-01-10 08:12:44 +0000

    Initial commit

---
+++ src/test/java/stepdefinition/FirstStepDefinition.java
@@ -0,0 +1,29 @@
+package stepdefinition;
+
+import io.cucumber.java.en.*;
+
+public class FirstStepDefinition {
+
+
+
+
+
+    @Given("Ilk feature icin cucumber indirdim.")
+    public void ilk_feature_icin_cucumber_indirdim() {
+        System.out.println("Given step i calisti");
+
+
+    }
+    @When("Seneryomu calistirdigimda")
+    public void seneryomu_calistirdigimda() {
+        System.out.println("When step i calisti");
+
+
+    }
+    @Then("Senaryomun calistigini konsolda gormeliyim.")
+    public void senaryomun_calistigini_konsolda_gormeliyim() {
+        System.out.println("Then step i calisti");
+
+    }
+
+}
---
+++ src/test/java/stepdefinition/TagsStepDefinition.java
@@ -0,0 +1,39 @@
+package stepdefinition;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TagsStepDefinition {
+    @Given("Smoke test icin hazirliklar yapildi")
+    public void smokeTestIcinHazirliklarYapildi() {
+
+    }
+
+    @When("Smoke test calistiginda")
+    public void smokeTestCalistiginda() {
+        System.out.println("Smoke test calisti");
+    }
+
+    @Then("Smoke test basarili olmali")
+    public void smokeTestBasariliOlmali() {
+    }
+
+    @Given("Regression test icin hazirliklar yapildi")
+    public void regressionTestIcinHazirliklarYapildi() {
+    }
+
+    @When("Regression test calistiginda")
+    public void regressionTestCalistiginda() {
+        System.out.println("Smoke test calisti");
+    }
+
+    @Then("Regression test basarili olmali")
+    public void regressionTestBasariliOlmali() {
+    }
+
+    @And("abc")
+    public void abc() {
+    }
+}
