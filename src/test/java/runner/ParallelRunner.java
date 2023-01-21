293ba3b8343548f5f98590535b3881bff4b74882
umitkalemkus <umitkalemkus@yahoo.com>
2023-01-21 22:27:05 +0000

    done

--- src/test/java/runner/ParallelRunner.java
+++ src/test/java/runner/ParallelRunner.java
@@ -11,6 +11,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = {"stepdefinitions", "hooks"}
+
+
 )
 public class ParallelRunner {
 }
